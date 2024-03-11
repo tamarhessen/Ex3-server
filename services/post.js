1dc9bc4044ed17625b37878ff8bdd70f07251ace
Daniel Lifshitz <68195849+Danlif1@users.noreply.github.com>
2024-03-11 12:51:30 +0200

    Deleting user now delete friends

--- services/post.js
+++ services/post.js
@@ -219,6 +219,7 @@ async function getAllPostsByUserId(userId, realUser) {
 }
 
 async function deleteUserById(userId) {
+    console.log("gsagsfsfd", userId);
     const displayName = (await User.findOne({username: userId})).displayName;
     console.log("displayName", displayName);
     const posts = await Post.deleteMany({CreatorUsername: userId});
@@ -230,11 +231,22 @@ async function deleteUserById(userId) {
             }
         })
     })
+    const all_users = await User.find();
+    await all_users.forEach(async (user) => {
+        user.friends.FriendList = user.friends.FriendList.filter(friend => friend !== userId)
+        user.friends.PendingList = user.friends.PendingList.filter(friend => friend !== userId)
+        if (user.username == userId) {
+
+        } else {
+            user.save();
+        }
+    })
     const user = await User.findOneAndRemove({username: userId})
     return user;
 }
 
 async function updateUserById(userId, newUsername, newImg, newDisplayName, newPassword) {
+    console.log("hello", userId)
     const user = await User.findOne({username: userId});
     if (!user) {
         console.log('user doesn\'t exist');
@@ -286,38 +298,53 @@ async function updateUserById(userId, newUsername, newImg, newDisplayName, newPa
         }
     }
     if (newImg) {
+        console.log("1")
         let posts = await Post.find();
+        console.log("2", posts.length)
         await posts.forEach(async (post) => {
+            console.log("2.5")
             if (post.Creator === user.displayName) {
+                console.log("3")
                 post.CreatorImg = newImg;
 
             }
             await post.Comments.forEach(async (comment) => {
+                console.log("4")
                 if (comment.creator === user.displayName) {
+                    console.log("5")
                     comment.creatorImg = newImg;
                 }
             })
+            console.log("6", post)
             await post.save();
+            console.log("7")
         })
+        console.log("8")
         let comments = await Comment.find();
+        console.log("9", comments.length)
         await comments.forEach(async (comment) => {
+            console.log("10")
             if (comment.creator === user.displayName) {
+                console.log("11")
                 comment.creatorImg = newImg;
                 await comment.save();
             }
         })
-
+        console.log("12")
         user.profilePic = newImg;
 
     }
     if (newDisplayName) {
+        console.log("helloo2", newDisplayName)
         if ((await User.findOne({displayName: newDisplayName}))) {
             console.log('couldn\'t change display name stopping process');
             return null
         }
         let posts = await Post.find();
         await posts.forEach(async (post) => {
+            console.log("yes")
             if (post.Creator === user.displayName) {
+                console.log("no?")
                 post.Creator = newDisplayName;
 
             }
@@ -328,20 +355,25 @@ async function updateUserById(userId, newUsername, newImg, newDisplayName, newPa
             })
             await post.save();
         })
+        console.log("here")
         let comments = await Comment.find();
+        console.log(comments)
         await comments.forEach(async (comment) => {
             if (comment.creator === user.displayName) {
                 comment.creator = newDisplayName;
                 await comment.save();
             }
         })
+        console.log("hereee")
         user.displayName = newDisplayName;
     }
 
     if (newPassword) {
         user.password = newPassword;
     }
+    console.log("lastone", user)
     await user.save();
+    console.log("no way")
     return user;
 }
 
